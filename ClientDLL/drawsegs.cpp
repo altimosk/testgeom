e69db154d5498c98457f84da71e68d64bcc4319a
alex <altimosk@gmail.com>
2015-12-11 10:49:53 -0800

    Update to comply with changes in the runner

--- ClientDLL/drawsegs.cpp
+++ ClientDLL/drawsegs.cpp
@@ -1,25 +1,25 @@
-#include "gengraph.h"
-static cGenericGraphics* GenericGraphics = 0;
+#include "..\gengraph.h"
+static GenericGraphics* GG = 0;
 extern "C"
-__declspec(dllexport) void SetGenericGraphics(cGenericGraphics *gg) {
-	GenericGraphics = gg;
+__declspec(dllexport) void SetGenericGraphics(GenericGraphics *gg) {
+	GG = gg;
 }
 
 
 extern "C"
 __declspec(dllexport) void DrawAll()
 {
-	if (!GenericGraphics)
+	if (!GG)
 		return;
 
-	GenericGraphics->Center(0, 0);
-	GenericGraphics->DrawLine(10, 10, 200, 50, kBlue, 10, false, true);
-	GenericGraphics->DrawLine(10, 50, 200, 20, kRed, 20, false, false);
-	GenericGraphics->DrawArc(100, 150, 200, 200, 40, 100, 200, kGold, 5, true, false);
-	GenericGraphics->DrawArc(100, 150, 200, 200, -40, 100, 200, kMagenta, 5, false, true);
+	GG->Center(0, 0);
+	GG->DrawLine(10, 10, 200, 50, kBlue, 10, false, true);
+	GG->DrawLine(10, 50, 200, 20, kRed, 20, false, false);
+	GG->DrawArc(100, 150, 200, 200, 40, 100, 200, kGold, 5, true, false);
+	GG->DrawArc(100, 150, 200, 200, -40, 100, 200, kMagenta, 5, false, true);
 
-	GenericGraphics->DrawRect( 10, 300, 100, 400, kGreen, false, true);
-	GenericGraphics->DrawRect(110, 300, 210, 400, kOrange, true, false);
+	GG->DrawRect(10, 300, 100, 400, kGreen, false, true);
+	GG->DrawRect(110, 300, 210, 400, kOrange, true, false);
 	
-	GenericGraphics->DrawCircle(100, 0, 40, kOrange, false, false);
+	GG->DrawCircle(100, 0, 40, kOrange, false, false);
 }
